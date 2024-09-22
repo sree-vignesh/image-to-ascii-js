aad835a6769781663ed990c45b9664dd77719301
Sree Vignesh <sreevignesh256@gmail.com>
2024-09-22 16:52:07 +0530

    Added a ' npm run show {filename} ' to quickly see the image.

--- printAscii.js
+++ printAscii.js
@@ -6,11 +6,12 @@ function printAscii(file) {
       colored: true,
       reverse: true,
       // pixels: " ▅",
-      pixels:"  ■■■■■",
+      pixels: "  ■■■■■",
     },
     (err, converted) => {
       console.log(err || converted);
     }
   );
 }
+// printAscii("abra.png");
 module.exports = { printAscii };
---
+++ show.js
@@ -0,0 +1,5 @@
+const args = process.argv.slice(2);
+// const fs = require("fs");
+const { printAscii } = require("./printAscii");
+
+printAscii(args);
